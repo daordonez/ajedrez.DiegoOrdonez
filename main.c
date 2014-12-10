5f9e335002f5cc40526dc7cc8b7cf31479b3ec63
Diego Ordóñez Morales <diegordonez4@gmail.com>
2014-12-10 23:39:09 +0100

    Inicio de proyecto
    Miércoles 10/12 primer commit de arranque

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ajedrez.DiegoOrdonez
+//
+//  Created by Diego Ordóñez Morales on 10/12/14.
+//  Copyright (c) 2014 Diego Ordóñez Morales. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
